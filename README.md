036e58936a752aac9122cc203148bab32f456bd5
2312747-刘玥彤 <moyuxiaotong@hotmail.com>
2026-05-17 22:26:38 +0800

    Update files on student branch

--- README.md
+++
@@ -1,5 +0,0 @@
-# git_homework_branch
-
-This is my software engineering homework project.
-B3 update
-B2 update
--- main.py
+++
@@ -1,5 +0,0 @@
-print("Hello Git")
-change from B3
-B3 different modification 1
-change from B2
-C4 modification 1
